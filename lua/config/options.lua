8ba264829a23763622d42c209d720b74f98381db
littlezabi <zohaibjozvi@gmail.com>
2026-08-01 16:05:49 +0500

    Fix options.lua and ensure unnamedplus clipboard setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- System clipboard integration
+vim.opt.clipboard = "unnamedplus"
+
 -- Line spacing (for GUI / supported terminals)
 vim.opt.linespace = 6
-
--- Neovide GUI specific line height
 vim.g.neovide_linespace = 6
